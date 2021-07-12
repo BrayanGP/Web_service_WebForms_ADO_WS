e9962dbf5374c8f58b3aa36e5bcafc62e487788d
Brayan23 <brayangp20@gmail.com>
2021-07-11 21:59:29 -0500

    Initial commit

---
+++ web_service/WebService1.asmx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace web_service
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        private string _cnnString;
+
+        public WebService1()
+        {
+            _cnnString = ConfigurationManager.ConnectionStrings["_web_service"].ConnectionString;
+        }
+
+        [WebMethod]
+        public string eliminar(int id)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(_cnnString))
+                {
+                    SqlCommand comando = new SqlCommand("sp_AlumnoDelete", con);
+                    comando.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    comando.Parameters.AddWithValue("id", id);
+                    int d = comando.ExecuteNonQuery();
+                    con.Close();
+
+                    if (d == 1)
+                    {
+                        return "Exito en la eliminacion";
+                    }
+                    else
+                    {
+                        return "Ningun registro afectado";
+                    }
+                }
+
+
+               
+            }
+            catch
+            {
+                return "Error en la eliminacion";
+            }
+
+        }
+
+        
+
+    }
+}
